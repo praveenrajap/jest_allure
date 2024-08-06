a2b149215264aadd0e31f560218462e203168759
praveenrajap <praveenrajap@microsoft.com>
2024-08-06 11:18:48 +0530

    test commit

--- __tests__/sample.spec.ts
+++ __tests__/sample.spec.ts
@@ -1,7 +1,24 @@
 const { it } = require("@jest/globals");
 const { label, link, issue, tms, step, attachment } = require("allure-js-commons");
 
-it("sample test", async () => {
+it("Navigate to about page [Allure Step]", async () => {
+  await label("foo", "bar");
+  await link("https://allurereport.org", "custom", "Allure Web-Site");
+  await issue("https://allurereport.org/fake-issue/1", "Issue 1");
+  await issue("2", "Issue 2");
+  await tms("https://allurereport.org/fake-task/1", "Task 1");
+  await tms("2", "Task 2");
+  await attachment("Sample text attachment", "Hello world!", "text/plain");
+  await step("step 1", async () => {
+    await step("step 1.1", async () => {
+      await step("step 1.1.1", async () => {
+        await attachment("Sample text attachment for step", "Goodbye world!", "text/plain");
+      });
+    });
+  });
+});
+
+it("test 2", async () => {
   await label("foo", "bar");
   await link("https://allurereport.org", "custom", "Allure Web-Site");
   await issue("https://allurereport.org/fake-issue/1", "Issue 1");
